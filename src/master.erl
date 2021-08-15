9a5bd94c8aa167699e4b4d131abcdaa98bf5ada0
Ruben Fratty <49823647+RubenFr@users.noreply.github.com>
2021-08-15 05:20:27 +0300

    Update master.erl
    - Added server up/down monitors
    - Handle server down -> choose randomly (hash) the server in charge to merge the data bast
    - Handler server up -> redistribute the data to ease the servers running
    - Reply of request modified

--- src/master.erl
+++ src/master.erl
@@ -18,9 +18,12 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
   code_change/3]).
 
--define(SERVER, master).
+-import(graph, [generate_graph/1, generate_graph/3]).
 
--record(master_state, {servers, namePID}).
+-define(SERVER, master).
+-define(SERVERS_FILENAME, "InputFiles/servers.txt").
+
+-record(master_state, {servers, namePID, upPID, downPID}).
 -record(request, {name, type, level}).
 
 %%%===================================================================
@@ -69,23 +72,23 @@ handle_call({name, NameID}, From, State = #master_state{}) ->
   case State#master_state.namePID of
     undefined -> {reply, not_available, State};
     PID -> 
-      spawn(fun() -> gen_server:reply(From, fetch_name(NameID, PID)) end),
+      spawn_monitor(fun() -> gen_server:reply(From, fetch_name(NameID, PID)) end),
       {noreply, State}
   end;
 
 %% Type = cast | title (movie/show/short)
 handle_call({request, Request = #request{}}, From, State = #master_state{}) ->
-  spawn(fun() -> process_request(Request, From, State#master_state.servers) end),
+  spawn_monitor(fun() -> process_request(Request, From, State#master_state.servers) end),
   {noreply, State};
 
 handle_call({request, Name, Type, Level}, From, State = #master_state{}) ->
   Request = #request{name = Name, type = Type, level = Level},
-  spawn(fun() -> process_request(Request, From, State#master_state.servers) end),
+  spawn_monitor(fun() -> process_request(Request, From, State#master_state.servers) end),
   {noreply, State};
 
 handle_call(request, From, State = #master_state{}) ->
   Request = #request{name = "Carmencita", type = title, level = 3},
-  spawn(fun() -> process_request(Request, From, State#master_state.servers) end),
+  spawn_monitor(fun() -> process_request(Request, From, State#master_state.servers) end),
   {noreply, State};
 
 handle_call(_Request, _From, State = #master_state{}) ->
@@ -104,20 +107,62 @@ handle_call(_Request, _From, State = #master_state{}) ->
 
 handle_cast(start, State = #master_state{}) ->
   Start = os:timestamp(),
-  {Servers, NamePID} = dataInit:start_distribution(),
-  broadcast(Servers, cast, stop_init),
-  io:format("Distributed data to ~p servers in ~p ms.~n",
-    [length(Servers), round(timer:now_diff(os:timestamp(), Start) / 1000)]),
-  {noreply, State#master_state{servers = Servers, namePID = NamePID}};
+
+  {NodesAlive, NamePID} = dataInit:start_distribution(),
+    
+  broadcast(NodesAlive, cast, stop_init),
+  UpPID = spawn(fun() -> check_server_up([], dataInit:servers() -- NodesAlive) end),
+  DownPID = spawn(fun() -> check_server_down([], NodesAlive) end),
+  
+  io:format("Distributed data to ~p servers in ~p ms.~n", [length(NodesAlive), round(timer:now_diff(os:timestamp(), Start) / 1000)]),
+  {noreply, State#master_state{servers = NodesAlive, namePID = NamePID, upPID = UpPID, downPID = DownPID}};
 
 handle_cast({distribute, Node, FileName}, State = #master_state{}) ->
   Reply = pmap:map(
     fun(S) -> 
       {ok, Bin} = file:read_file(FileName),
-      ok = rpc:call(S, file, write_file, [FileName, Bin]) end,
+      ok = rpc:call(S, file, write_file, [FileName, Bin])
+      % ok = file:delete(FileName) 
+    end,
     State#master_state.servers -- [Node]),
-  io:format("Distributed data from ~p to other servers.~n", [Node]),
+  io:format("Distributed data from ~p to other servers -> ~p~n", [Node, Reply]),
   {noreply, State};
+
+handle_cast({server_up, Node},  State = #master_state{}) ->
+  NewList = [Node | State#master_state.servers],
+
+  State#master_state.downPID ! {new_node, Node}, % Update the list of servers to check if down
+  broadcast(NewList, pcall, reset),           % Tell to all the servers to discard the data they had until now
+  dataInit:distribute(NewList),               % Distribute the data to the servers in NewList
+  broadcast(NewList, cast, stop_init),     % Tell to all the servers the init is finished
+
+  io:format("~nNew Server is UP: ~p\t~p Server(s) Running!~n~n", [Node, length(NewList)]),
+  {noreply, State#master_state{servers = NewList}};
+
+handle_cast({server_down, Node},  State = #master_state{}) ->
+  NewList = State#master_state.servers -- [Node],
+
+  State#master_state.upPID ! {new_node, Node}, % Update the list of servers to check if up
+  dataInit:redistribute(NewList, Node),     % Tell to one of the servers running to handle the data of the server down
+  
+  io:format("~nServer ~p is DOWN\t~p Server(s) Running!~n~n", [Node, length(NewList)]),
+  {noreply, State#master_state{servers = NewList}};
+
+% handle_cast(test, State = #master_state{}) ->
+%   spawn_monitor(
+%     fun() ->
+%       G = digraph:new(),
+%       V1 = digraph:add_vertex(G, "A","A"),
+%       V2 = digraph:add_vertex(G, "B","B"),
+%       V3 = digraph:add_vertex(G, "C","C"),
+%       E1 = digraph:add_edge(G, V1, V2),
+%       E2 = digraph:add_edge(G, V1, V3),
+%       FileName = graph:generate_graph(G),
+%       Reply = os:cmd(io_lib:format("xdg-open ~s", [FileName])),
+%       io:format("Opening file ~s: ~p~n", [FileName, Reply])
+%     end
+%   ),
+%   {noreply, State};
 
 handle_cast(_Request, State = #master_state{}) ->
   {noreply, State}.
@@ -173,6 +218,7 @@ fetch_name(NameID, DB) ->
     Name -> Name
   end.
 
+broadcast([], _, _) -> ok;
 broadcast(Servers, Type, Message) ->
   case Type of
     cast -> lists:foreach(fun(S) -> gen_server:cast({server, S}, Message) end, Servers);
@@ -184,18 +230,22 @@ broadcast(Servers, Type, Message) ->
 
 process_request(Request = #request{}, From, Servers) ->
   io:format("Received a new Request: Name = ~p\tType = ~p\tLevel = ~p~n", [Request#request.name, Request#request.type, Request#request.level]),
-  G = digraph:new([acyclic, protected]),
+  G = digraph:new([protected]),
   Root = digraph:add_vertex(G, Request#request.name, Request#request.name),
   Set = sets:new(),
   process_request(G, Root, sets:add_element(Request#request.name, Set), Request, 1, Servers),
-  gen_server:reply(From, G).
+  gen_server:reply(From, {length(digraph:edges(G)), length(digraph:vertices(G))}),
+  % FileName = generate_graph(G, Root, Request#request.type),
+  FileName = generate_graph(G),
+  Reply = os:cmd(io_lib:format("xdg-open ~s", [FileName])),
+  io:format("Opening file ~s: ~p~n", [FileName, Reply]).
 
 process_request(_, _, _, Request = #request{}, Level, _) when Level =:= Request#request.level -> ok;
 
 process_request(G, V, Set, Request = #request{}, Level, Servers) ->
   Children = parse_reply(broadcast(Servers, pcall, {request, Request#request{name = V, level = Level}})),
   {AddedVertices, UpdatedSet} = update_graph(G, V, Set, Children, []),
-  io:format("Level ~p: ~p~n", [Level, Children]),
+  io:format("Level ~p: ~s~n", [Level, string:join(AddedVertices, ", ")]),
   lists:foreach(fun(Vertex) -> process_request(G, Vertex, UpdatedSet, Request, Level + 1, Servers) end, AddedVertices).
 
 
@@ -209,8 +259,56 @@ update_graph(G, V, Set, [Name | Next], Vertices) ->
       update_graph(G, V, sets:add_element(Name, Set), Next, [V1 | Vertices])
   end.
 
+
 parse_reply(List) -> parse_reply(List, []).
 parse_reply([], Reply) -> sets:to_list(sets:from_list(Reply));
 parse_reply([badarg | _], _) -> badarg;
 parse_reply([notfound | Next], Reply) -> parse_reply(Next, Reply);
 parse_reply([R | Next], Reply) -> parse_reply(Next, R ++ Reply). 
+
+
+check_server_up([], []) ->
+  receive
+    NewNode when is_atom(NewNode) -> check_server_up(NewNode, [])
+  end;
+
+check_server_up([], Servers) -> check_server_up(Servers, []);
+
+check_server_up([ToCheck | Next], Servers) ->
+  case net_kernel:connect_node(ToCheck) of
+    true -> 
+      % io:format("Server ~p is UP!~n", [ToCheck]),
+      gen_server:cast({master, node()}, {server_up, ToCheck}),
+      check_server_up(Next, Servers);
+    false -> 
+      receive
+        {new_node, NewNode} -> 
+          % io:format("Up Checker: New Node to check ~p~n", [NewNode]),
+          check_server_up(Next, [ToCheck, NewNode | Servers])
+      after
+        1000 -> check_server_up(Next, [ToCheck | Servers])
+      end
+  end.
+
+check_server_down([], []) ->
+  receive
+    {new_node, NewNode} -> check_server_down(NewNode, [])
+  end;
+
+check_server_down([], Servers) -> check_server_down(Servers, []);
+
+check_server_down([ToCheck | Next], Servers) ->
+  case net_kernel:connect_node(ToCheck) of
+    false -> 
+      % io:format("Server ~p is DOWN!~n", [ToCheck]),
+      gen_server:cast({master, node()}, {server_down, ToCheck}),
+      check_server_down(Next, Servers);
+    true -> 
+      receive
+        {new_node, NewNode} -> 
+          % io:format("Down Checker: New Node to check ~p~n", [NewNode]),
+          check_server_down(Next, [ToCheck, NewNode | Servers])
+      after
+        1000 -> check_server_down(Next, [ToCheck | Servers])
+      end
+  end.
