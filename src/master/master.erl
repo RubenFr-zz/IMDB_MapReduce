628296d825441911b90118bc254c65370bdb4953
Ruben Fratty <49823647+RubenFr@users.noreply.github.com>
2021-08-18 12:05:43 +0300

    Added comments

--- src/master/master.erl
+++ src/master/master.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %%% @author Ruben
-%%% @copyright (C) 2021, <COMPANY>
+%%% @copyright (C) 2021, BGU
 %%% @doc
 %%%
 %%% @end
@@ -164,8 +164,12 @@ handle_info(_Info, State = #master_state{}) ->
 %% with Reason. The return value is ignored.
 -spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
     State :: #master_state{}) -> term()).
-terminate(_Reason, State = #master_state{monitor = Pid}) ->
-  Pid ! stop,
+terminate(Reason, State = #master_state{}) ->
+  io:format("MASTER DOWN~n~p~n", [Reason]),
+  case State#master_state.monitor of
+    undefined -> ignore;
+    Pid -> Pid ! stop
+  end,
   broadcast(State#master_state.servers, cast, master_down).
 
 %%%===================================================================
@@ -181,16 +185,35 @@ code_change(_OldVsn, State = #master_state{}, _Extra) ->
   {ok, State}.
 
 %%%===================================================================
-%%% Internal functions
+%%% Internal functions - fetch_name
 %%%===================================================================
 
+%% @doc Return the name associated to the NameId stored in the DB
+-spec fetch_name(NameId :: string(), DB :: pid()) -> Name :: string() | not_found.
 fetch_name(NameID, DB) ->
   DB ! {self(), NameID},
   receive
     Name -> Name
   end.
 
-broadcast([], _, _) -> ok;
+%%%===================================================================
+%%% Internal functions - broadcast
+%%%===================================================================
+
+%% @doc Broadcast a Message to the Servers.
+%% cast -> cast the message to the nodes
+%% call -> serial call to the servers -> return a list of the replies
+%% pcall -> parallel call to the servers -> return a list of the replies (in the same order)
+-spec broadcast(Servers, Type, Message) ->
+  ok | [Replies] when
+  Servers :: [S],
+  S :: atom(),
+  Type :: cast | call | pcall,
+  Replies :: term(),
+  Message :: term().
+
+broadcast([], pcall, _) -> [];  %% List of servers empty
+broadcast([], _, _) -> ok;  %% List of servers empty
 broadcast(Servers, Type, Message) ->
   io:format("Sending Message (~p) to ~p (~p servers).~n", [Message, Servers, length(Servers)]),
   case Type of
@@ -200,6 +223,12 @@ broadcast(Servers, Type, Message) ->
     _ -> io:format("ERROR")
   end.
 
+%%%===================================================================
+%%% Internal functions - process_request
+%%%===================================================================
+
+%% @doc Process a request from a client. Build and display a graphviz
+-spec process_request(Request :: #request{}, From :: {Pid :: pid(), Tag :: term()}, Servers :: [atom()]) -> ok.
 
 process_request(Request = #request{}, From, Servers) ->
   io:format("Received a new Request: Name = ~p\tType = ~p\tLevel = ~p~n", [Request#request.name, Request#request.type, Request#request.level]),
@@ -220,7 +249,11 @@ process_request(G, V, Set, Request = #request{}, Level, Servers) ->
   io:format("Level ~p: ~s~n", [Level, string:join(AddedVertices, ", ")]),
   lists:foreach(fun(Vertex) -> process_request(G, Vertex, UpdatedSet, Request, Level + 1, Servers) end, AddedVertices).
 
+%%%===================================================================
+%%% Internal functions - update_graph
+%%%===================================================================
 
+%% @doc Add new vertices and edges to graph G
 update_graph(_, _, Set, [], V) -> {V, Set};
 update_graph(G, V, Set, [Name | Next], Vertices) ->
   case sets:is_element(Name, Set) of
@@ -231,14 +264,26 @@ update_graph(G, V, Set, [Name | Next], Vertices) ->
       update_graph(G, V, sets:add_element(Name, Set), Next, [V1 | Vertices])
   end.
 
+%%%===================================================================
+%%% Internal functions - parse_reply
+%%%===================================================================
+
+%% @doc Process a request from a client. Build and display a graphviz
+-spec parse_reply(List :: [string()]) -> [string()].
+-spec parse_reply(List :: [string()], Reply :: [string()]) -> [string()].
 
 parse_reply(List) -> parse_reply(List, []).
 parse_reply([], Reply) -> sets:to_list(sets:from_list(Reply));
 parse_reply([badarg | _], _) -> badarg;
-parse_reply([notfound | Next], Reply) -> parse_reply(Next, Reply);
+parse_reply([not_found | Next], Reply) -> parse_reply(Next, Reply);
 parse_reply([R | Next], Reply) -> parse_reply(Next, R ++ Reply).
 
+%%%===================================================================
+%%% Internal functions - distribute_files
+%%%===================================================================
 
+%% @doc Process a request from a client. Build and display a graphviz
+-spec distribute_files(Node :: atom(), FileName :: string(), Servers :: [atom()], From :: {Pid :: pid(), Tag :: term()}) -> ok.
 distribute_files(Node, FileName, Servers, From) ->
   {ok, Bin} = file:read_file(FileName),
   Reply = pmap:map(
